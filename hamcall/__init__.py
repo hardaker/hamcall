eccd8918a66bd819d853052422420fcf4b4c2df6
Wes Hardaker <opensource@hardakers.net>
2024-06-19 10:16:19 -0700

    reformat and create indexes

--- hamcall/__init__.py
+++ hamcall/__init__.py
@@ -1,11 +1,10 @@
-"""Create and access a local FCC ULS database of ham callsigns"""
+"""Create and access a local FCC ULS database of ham callsigns."""
 
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from logging import debug, info, warning, error, critical
-import logging
-import sys
-import sqlite3
 import json
+import logging
+import sqlite3
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
+from logging import debug, info
 from pathlib import Path
 
 __VERSION__ = "0.1"
@@ -14,17 +13,22 @@ __VERSION__ = "0.1"
 try:
     from rich import print
     from rich.logging import RichHandler
-except Exception:
-    pass
+except ModuleNotFoundError:
+    debug("you might add rich and rich.logging modules for better output")
 
 # optionally use rich_argparse too
 help_handler = ArgumentDefaultsHelpFormatter
 try:
     from rich_argparse import RichHelpFormatter
-    help_handler = RichHelpFormatter
-except Exception:
-    pass
 
+    help_handler = RichHelpFormatter
+except ModuleNotFoundError:
+    debug("you might add rich_argparse for better help output")
+
+# other possibilities:
+# "grant_date": "Granted",
+# "expired_date": "Expires",
+# "cancellation_date": "Canceled",
 default_fields = {
     "unique_system_identifier": "Id",
     "callsign": "Callsign",
@@ -32,9 +36,6 @@ default_fields = {
     "previous_callsign": "Previously",
     "last_name": "Last name",
     "first_name": "First name",
-#    "grant_date": "Granted",
-#    "expired_date": "Expires",
-#    "cancellation_date": "Canceled",
     "street_address": "Address",
     "po_box": "PO Box",
     "city": "City",
@@ -42,37 +43,73 @@ default_fields = {
     "zip_code": "Zip code",
 }
 
-def parse_args():
-    "Parse the command line arguments."
-    parser = ArgumentParser(formatter_class=help_handler,
-                            description=__doc__,
-                            epilog="Example Usage: ")
 
-    parser.add_argument("-d", "--database", default=Path.home()  / ".hampy.db",
-                        type=str,
-                        help="Where to store the database")
+def parse_args() -> Namespace:
+    """Parse the command line arguments."""
+    parser = ArgumentParser(
+        formatter_class=help_handler,
+        description=__doc__,
+        epilog="Example Usage: ",
+    )
 
-    parser.add_argument("-i", "--init", action="store_true",
-                        help="Create the database structure.")
+    parser.add_argument(
+        "-d",
+        "--database",
+        default=Path.home() / ".hampy.db",
+        type=str,
+        help="Where to store the database",
+    )
 
-    parser.add_argument("-l", "--load", default=None, type=str,
-                        help="Load this directory of files into the database")
+    parser.add_argument(
+        "-i",
+        "--init",
+        action="store_true",
+        help="Create the database structure.",
+    )
 
-    parser.add_argument("-f", "--display_fields", default=default_fields,
-                        type=list[str], help="List of DB fields to show")
+    parser.add_argument(
+        "-l",
+        "--load",
+        default=None,
+        type=str,
+        help="Load this directory of files into the database",
+    )
 
-    parser.add_argument("-a", "--all-fields", action="store_true",
-                        help="Just dump all fields")
+    parser.add_argument(
+        "-f",
+        "--display_fields",
+        default=default_fields,
+        type=list[str],
+        help="List of DB fields to show",
+    )
 
-    parser.add_argument("-j", "--json", action="store_true",
-                        help="Dump all found records in JSON format")
+    parser.add_argument(
+        "-a",
+        "--all-fields",
+        action="store_true",
+        help="Just dump all fields",
+    )
 
-    parser.add_argument("--log-level", "--ll", default="info",
-                        help="Define the logging verbosity level (debug, info, warning, error, fotal, critical).")
+    parser.add_argument(
+        "-j",
+        "--json",
+        action="store_true",
+        help="Dump all found records in JSON format",
+    )
 
-    parser.add_argument("callsigns", type=str,
-                        nargs='*',
-                        help="Amateur radio callsigns to look up")
+    parser.add_argument(
+        "--log-level",
+        "--ll",
+        default="info",
+        help="Define the logging verbosity level (debug, info, warning, error, fotal, critical).",
+    )
+
+    parser.add_argument(
+        "callsigns",
+        type=str,
+        nargs="*",
+        help="Amateur radio callsigns to look up",
+    )
 
     args = parser.parse_args()
     log_level = args.log_level.upper()
@@ -86,21 +123,26 @@ def parse_args():
         datefmt = " "
         messagefmt = "%(message)s"
     except Exception:
-        pass
+        debug("failed to use RichHandler")
 
-    logging.basicConfig(level=log_level,
-                        format=messagefmt,
-                        datefmt=datefmt,
-                        handlers=handlers)
+    logging.basicConfig(
+        level=log_level,
+        format=messagefmt,
+        datefmt=datefmt,
+        handlers=handlers,
+    )
     return args
+
 
 def get_db(args):
     return sqlite3.connect(args.database)
 
+
 def create_db(args):
     db = get_db(args)
 
-    db.execute("""create table PUBACC_AM
+    db.execute(
+        """create table PUBACC_AM
 (
       record_type               char(2)              not null,
       unique_system_identifier  numeric(9,0)         not null,
@@ -120,9 +162,11 @@ def create_db(args):
       previous_callsign         char(10)             null,
       previous_operator_class   char(1)              null,
       trustee_name              varchar(50)          null
-)""")
+)""",
+    )
 
-    db.execute("""create table PUBACC_EN
+    db.execute(
+        """create table PUBACC_EN
 (
       record_type               char(2)              not null,
       unique_system_identifier  numeric(9,0)         not null,
@@ -154,8 +198,10 @@ def create_db(args):
       lic_category_code char(1)     null,
       linked_license_id numeric(9,0)    null,
       linked_callsign       char(10)        null
-)""")
-    db.execute("""create table PUBACC_HD
+)""",
+    )
+    db.execute(
+        """create table PUBACC_HD
 (
       record_type               char(2)              not null,
       unique_system_identifier  numeric(9,0)         not null,
@@ -215,12 +261,21 @@ def create_db(args):
       eligibility_cert_900        char(1)              null,
       transition_plan_cert_900    char(1)              null,
       return_spectrum_cert_900  char(1)              null,
-      payment_cert_900        char(1)              null)""")
+      payment_cert_900        char(1)              null)""",
+    )
 
-    info(f"created database table at {args.database}")
-    
+    info(f"created database tables in {args.database}")
+
+    db.execute("create index am_callsign on PUBACC_AM (callsign)")
+    db.execute("create index am_uls on PUBACC_AM (unique_system_identifier)")
+    db.execute("create index en_uls on PUBACC_EN (unique_system_identifier)")
+    db.execute("create index hd_uls on PUBACC_HD (unique_system_identifier)")
+
+    info(f"created database indexes in {args.database}")
+
+
 def load_file_into_table(db, filename: str, table: str, columns: int):
-    values = ("?, " * (columns-1)) + "?"
+    values = ("?, " * (columns - 1)) + "?"
     with open(filename) as lh:
         for n, line in enumerate(lh):
             pieces = line.strip().split("|")
@@ -228,6 +283,7 @@ def load_file_into_table(db, filename: str, table: str, columns: int):
             if n % 10000 == 0:
                 info(f"loading table {table}: {n}")
     db.commit()
+
 
 def load_db(args):
     db = get_db(args)
@@ -237,11 +293,13 @@ def load_db(args):
     load_file_into_table(db, dir.joinpath("HD.dat"), "HD", 59)
     db.commit()
 
+
 def row_to_dict(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
+
 
 def display_callsign(args, result):
     if args.json:
@@ -249,7 +307,7 @@ def display_callsign(args, result):
         return
 
     if args.all_fields:
-        args.display_fields = {x:x for x in result.keys()}
+        args.display_fields = {x: x for x in result}
 
     print(f"{result['callsign']}:")
     for field in args.display_fields:
@@ -257,28 +315,36 @@ def display_callsign(args, result):
             label = args.display_fields[field]
             print(f"  {label:<20}: {result[field]}")
 
+
 def lookup_callsigns(args):
     db = get_db(args)
     for callsign in args.callsigns:
-
         # fetch the amateur record
         debug(f"searching for callsign {callsign}")
-        cursor = db.execute("select * from PUBACC_AM where callsign = ?",
-                            [callsign.upper()])
+        cursor = db.execute(
+            "select * from PUBACC_AM where callsign = ?",
+            [callsign.upper()],
+        )
         cursor.row_factory = row_to_dict
         results = cursor.fetchall()
 
         # for each result, also fetch additional data from the other databases:
         for result in results:
-            id = result['unique_system_identifier']
+            id = result["unique_system_identifier"]
 
-            en_handle = db.execute("select * from PUBACC_EN where unique_system_identifier = ?", [id])
+            en_handle = db.execute(
+                "select * from PUBACC_EN where unique_system_identifier = ?",
+                [id],
+            )
             en_handle.row_factory = row_to_dict
             additionals = en_handle.fetchall()
             for additional in additionals:
                 result.update(additional)
 
-            en_handle = db.execute("select * from PUBACC_HD where unique_system_identifier = ?", [id])
+            en_handle = db.execute(
+                "select * from PUBACC_HD where unique_system_identifier = ?",
+                [id],
+            )
             en_handle.row_factory = row_to_dict
             additionals = en_handle.fetchall()
             for additional in additionals:
@@ -286,6 +352,7 @@ def lookup_callsigns(args):
 
         for result in results:
             display_callsign(args, result)
+
 
 def main():
     args = parse_args()
@@ -297,8 +364,9 @@ def main():
     if args.load:
         load_db(args)
         return
-        
+
     lookup_callsigns(args)
+
 
 if __name__ == "__main__":
     main()
